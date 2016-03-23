57491000a901607e33d9a524e9b5b4e7dec54672
chat-wane <grumpy.chat.wane@gmail.com>
2016-03-23 17:12:48 +0100

    meow

--- example/main.js
+++ example/main.js
@@ -33,17 +33,16 @@ var id2 = n1.connection(callbacks(n1, n3));
 
 // #3 n1 chooses to connect n2 to n3 (neighbor2neighbor connection)
 function bridge(){
-//    console.log(n1);
-    entry1 = n1.outview.living.ms.arr[0]; // ugly
-    entry2 = n1.outview.living.ms.arr[1]; // probably better way
-    console.log(entry1.id+ ' -> ' +entry2.id);
-    n1.connect(entry1.id, entry2.id);
+     entry1 = n1.outview.living.ms.arr[0]; // ugly
+     entry2 = n1.outview.living.ms.arr[1]; // probably better way
+     console.log(entry1.id+ ' -> ' +entry2.id);
+     n1.connect(entry1.id, entry2.id);
 
-    // #4 add a direct connection from n2 to n1 (direct connection)
-    n2.connect(null, n1.outview.ID);
-    n2.connect(null, n1.outview.ID); // x2 but no additionnal connection
-    // #5 add an arc from n1 to n2 at the initiative of n2
-    n2.connect(n1.outview.ID);
+//     // #4 add a direct connection from n2 to n1 (direct connection)
+     n2.connect(null, n1.outview.ID);
+     n2.connect(null, n1.outview.ID); // x2 but no additionnal connection
+//     // #5 add an arc from n1 to n2 at the initiative of n2
+     n2.connect(n1.outview.ID);
 };
 
 
